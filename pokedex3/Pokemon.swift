14e2ea602a59e046767e8a36e38a2e2ee06ec85a
swift2geek <swift2geek@gmail.com>
2017-03-30 16:40:56 +0300

    finaly works

--- pokedex3/Pokemon.swift
+++ pokedex3/Pokemon.swift
@@ -21,7 +21,37 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionText: String!
+    private var _nextEvolutionName: String!
+    private var _nextEvolutionId: String!
+    private var _nextEvolutionLvl: String!
     private var _pokemonURL: String!
+    
+    var nextEvolutionLvl: String {
+        
+        if _nextEvolutionLvl == nil {
+            
+            _nextEvolutionLvl = ""
+        }
+        return _nextEvolutionLvl
+    }
+    
+    var nextEvolutionName: String {
+        
+        if _nextEvolutionName == nil {
+            
+            _nextEvolutionName = ""
+        }
+        return _nextEvolutionName
+    }
+    
+    var nextEvolutionId: String {
+        
+        if _nextEvolutionId == nil {
+            
+            _nextEvolutionId = ""
+        }
+        return _nextEvolutionId
+    }
     
     var description: String {
         
@@ -159,6 +189,72 @@ class Pokemon {
                     
                     self._type = ""
                 }
+                
+                if let descArr = dict["descriptions"] as? [Dictionary<String, String>] , descArr.count > 0 {
+                    
+                    if let url = descArr[0]["resource_uri"] {
+                        
+                        let descURL = URL_BASE + url
+                        
+                        Alamofire.request(descURL, method: .get).responseJSON(completionHandler: { (response) in
+                            
+                            if let descDict = response.result.value as? Dictionary<String, AnyObject> {
+                                
+                                if let description = descDict["description"] as? String {
+                                    
+                                    let newDescription = description.replacingOccurrences(of: "POKMON", with: "Pokemon")
+                                    
+                                    self._description = newDescription
+                                    print(newDescription)
+                                }
+                            }
+                            
+                            completed()
+                            
+                        })
+                    }
+                } else {
+                    
+                    self._description = ""
+                }
+                
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] , evolutions.count > 0 {
+                    
+                    if let nextEvo = evolutions[0]["to"] as? String {
+                        
+                        if nextEvo.range(of: "mega") == nil {
+                            
+                            self._nextEvolutionName = nextEvo
+                            
+                            if let uri = evolutions[0]["resource_uri"] as? String {
+                                
+                                let newStr = uri.replacingOccurrences(of: "/api/v1/pokemon/", with: "")
+                                let nextEvoId = newStr.replacingOccurrences(of: "/", with: "")
+                                
+                                self._nextEvolutionId = nextEvoId
+                                
+                                if let lvlExist = evolutions[0]["level"] {
+                                    
+                                    if let lvl = lvlExist as? Int {
+                                        self._nextEvolutionLvl = String(lvl)
+                                    }
+                                    
+                                } else {
+                                    
+                                    self._nextEvolutionLvl = ""
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                    print(self.nextEvolutionLvl)
+                    print(self.nextEvolutionName)
+                    print(self.nextEvolutionId)
+                }
+                
             }
             
             completed()
